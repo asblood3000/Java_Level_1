5bf2adb132afca84ed909c18eec2dd4ba072b3bd
asblood3000 <asblood3000@gmail.com>
2022-05-12 00:30:32 +0300

    just commit

--- src/Lesson3/Lesson3.java
+++ src/Lesson3/Lesson3.java
@@ -2,9 +2,6 @@ package Lesson3;
 
 public class Lesson3 {
     public static void main(String[] args) {
-        //change0To1();
-        //fillArray();
-        myMassiv();
     }
 
     public static void change0To1 () {
@@ -27,12 +24,45 @@ public class Lesson3 {
         }
     }
 
-    public static void myMassiv() {
+    public static void changeArray() {
         int[] arr = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < 6) {
-                arr[i] = arr[i] * 2;
+                arr[i] *= 2;
             }
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void fillDiagonal() {
+        int[][] arr = new int[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (i == j) {
+                    arr[i][j] = 1;
+                }
+                else {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("[");
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + "  ");
+            }
+            System.out.print("\b\b]");
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void returnLengthArray() {
+        int len = 3;
+        int initialValue = 3333;
+        int[] arr = new int[len];
+        for (int i = 0; i < arr.length; i++){
+            arr[i] = initialValue;
             System.out.println(arr[i]);
         }
     }
