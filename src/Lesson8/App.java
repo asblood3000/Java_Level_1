684be5467ec5ef0b1cce2a0dbcf44f060d0861b4
asblood3000 <asblood3000@gmail.com>
2022-06-04 22:47:57 +0300

    just commit

---
+++ src/Lesson8/App.java
@@ -0,0 +1,12 @@
+package Lesson8;
+
+import javax.swing.*;
+
+public class App extends JFrame {
+    public App() {
+        setTitle("Моя программа");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(100, 100, 100, 100);
+        setVisible(true);
+    }
+}
