8953ec3231341a118c664dcfb1d262c316ce7c2d
asblood3000 <asblood3000@gmail.com>
2022-06-04 23:17:42 +0300

    just commit

--- src/Lesson8/App.java
+++ src/Lesson8/App.java
@@ -1,12 +1,51 @@
 package Lesson8;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class App extends JFrame {
-    public App() {
-        setTitle("Моя программа");
+    private int value;
+    public App(int initialValue) {
+        setBounds(500, 500, 300, 120);
+        setTitle("Simple Counter");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setBounds(100, 100, 100, 100);
+
+        Font font = new Font("Arial", Font.BOLD, 32);
+
+        JLabel counterValueView = new JLabel();
+        counterValueView.setFont(font);
+        counterValueView.setHorizontalAlignment(SwingConstants.CENTER);
+        add(counterValueView, BorderLayout.CENTER);
+
+        value = initialValue;
+        counterValueView.setText(String.valueOf(value));
+
+        JButton decrementButton = new JButton("<");
+        decrementButton.setFont(font);
+        add(decrementButton, BorderLayout.WEST);
+
+        JButton incrementButton = new JButton(">");
+        incrementButton.setFont(font);
+        add(incrementButton, BorderLayout.EAST);
+
+        decrementButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                value--;
+                counterValueView.setText(String.valueOf(value));
+            }
+        });
+
+        incrementButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                value++;
+                counterValueView.setText(String.valueOf(value));
+            }
+        });
+
         setVisible(true);
     }
 }
--- src/Lesson8/Main.java
+++ src/Lesson8/Main.java
@@ -2,6 +2,6 @@ package Lesson8;
 
 public class Main {
     public static void main(String[] args) {
-
+        new App(0);
     }
 }
