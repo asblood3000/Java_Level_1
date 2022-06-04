5792ad73e27c54188836e5181303b31e483147df
asblood3000 <asblood3000@gmail.com>
2022-06-04 17:09:29 +0300

    just commit

--- src/Lesson7/Lesson7.java
+++ src/Lesson7/Lesson7.java
@@ -2,6 +2,15 @@ package Lesson7;
 
 public class Lesson7 {
     public static void main(String[] args) {
-
+            Cat[] cat = new Cat[2];
+            cat[0] = new Cat("Том", 25);
+            cat[1] = new Cat("Питер", 85);
+            Plate plate = new Plate(100);
+            for (int i = 0; i < cat.length; i++) {
+                cat[i].info();
+                plate.info();
+                cat[i].eat(plate);
+                System.out.println();
+            }
     }
 }
